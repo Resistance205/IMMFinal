bb2cb35327eeaf0279bb3f12bb6ed17673dfcb7c
Resistance205 <B00137931@mytudublin.ie>
2021-12-12 20:14:05 +0000

    updates

--- Main/Beta_Build/Assets/Scripts/PlayerController.cs
+++ Main/Beta_Build/Assets/Scripts/PlayerController.cs
@@ -2,49 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController characterController;
-    public float moveSpeed = 30.0f;
+    public float horizontalInput;
+    public float moveSpeed = 10.0f;
     public bool isOnGround = true;
     private Rigidbody playerRb;
-    public float jumpForce = 12.4f;
+    public float jumpForce = 8;
     public bool gameOver;
     public AudioClip jumpSound;
     public AudioClip deathSound;
     public AudioClip winSound;
     private AudioSource playerAudio;
+    public float jumpSpeed = 30.0F;
+    public float gravity = 1.0F;
+    private Vector3 moveDirection = Vector3.zero;
 
-    public float gravModifier = 20.0f;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAudio = GetComponent<AudioSource>();
-        characterController = gameObject.AddComponent<CharacterController>();
-        Physics.gravity = new Vector3(0.0f, -gravModifier, 0.0f);
+
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (gameOver)
+        if (!gameOver)
         {
-            return;
-        }
+            CharacterController controller = GetComponent<CharacterController>();
+            Vector3 forward = transform.TransformDirection(Vector3.right);
+            float curSpeed = moveSpeed * Input.GetAxis("Horizontal");
+            controller.SimpleMove(forward * curSpeed);
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        characterController.Move(Vector3.right * horizontalInput * Time.deltaTime * moveSpeed);
+            if (controller.isGrounded && Input.GetButton("Jump"))
+            {
+                moveDirection.y = 20;
+            }
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        } // referenced from https://docs.unity3d.com/ScriptReference/CharacterController.Move.html
 
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
-        {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
-            playerAudio.PlayOneShot(jumpSound, 1.0f);
 
-        }
     }
+
+
+
 
     private void OnCollisionEnter(Collision collision)
     {
